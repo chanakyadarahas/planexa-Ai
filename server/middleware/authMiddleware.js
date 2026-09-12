5427699947cdd0895c9f865e13d29f462df0ca80
Aila Chanakya Darahas <chanakyadarahas@gmail.com>
2026-09-12 18:40:28 -0400

    Remove JWT debugging

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -21,7 +21,7 @@ const authMiddleware = (req, res, next) => {
 
     next();
   } catch (error) {
-  console.error("JWT ERROR:", error.message);
+ 
 
   return res.status(401).json({
     error: "Invalid or expired token",
